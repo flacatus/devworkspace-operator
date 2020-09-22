2678f926b6b25efd56cfa0fd58527ee3f79a10e6
Flavius Lacatusu <flacatus@redhat.com>
2020-09-22 15:13:14 +0200

    Fix script description
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .cico/cico_openshift_e2e.sh
+++ .cico/cico_openshift_e2e.sh
@@ -34,7 +34,8 @@ export NAMESPACE="devworkspace-controller"
 # Component is defined in Openshift CI job configuration. See: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v4.yaml#L8
 export CI_COMPONENT="devworkspace-operator"
 
-# OPENSHIFT_BUILD_NAMESPACE env var exposed by Openshift CI. More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
+# DEVWORKSACE_OPERATOR env var exposed by Openshift CI in e2e test pod. More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
+# Dependencies environment are defined here: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v5.yaml#L36-L38
 export IMG=${DEVWORKSACE_OPERATOR}
 
 # Pod created by openshift ci don't have user. Using this envs should avoid errors with git user.
@@ -48,9 +49,10 @@ if ! hash operator-sdk 2>/dev/null; then
     OPERATOR_SDK_VERSION=v0.17.0
 
     curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu
+
     chmod +x operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu && \
-    cp operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu $GOPATH/bin/operator-sdk && \
-    rm operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu
+        cp operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu $GOPATH/bin/operator-sdk && \
+        rm operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu
 fi
 
 # Function to get all logs and events from Che deployments
